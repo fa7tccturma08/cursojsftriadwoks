338ed6dc53841b6eb6da1a9409712b992330a035
Savio Rodrigues <saviojrc.1988@hotmail.com>
2017-04-25 21:27:43 -0300

    Terminei o cadastro de bugs

--- src/br/com/triadworks/bugtracker/controller/BugBean.java
+++ src/br/com/triadworks/bugtracker/controller/BugBean.java
@@ -8,8 +8,10 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.triadworks.bugtracker.dao.BugDao;
+import br.com.triadworks.bugtracker.dao.UsuarioDao;
 import br.com.triadworks.bugtracker.modelo.Bug;
 import br.com.triadworks.bugtracker.modelo.Status;
+import br.com.triadworks.bugtracker.modelo.Usuario;
 import br.com.triadworks.bugtracker.util.Constantes;
 import br.com.triadworks.bugtracker.util.FacesUtils;
 import br.com.triadworks.bugtraker.interceptor.Transacional;
@@ -22,6 +24,11 @@ public class BugBean {
 	@Inject
 	private BugDao bugDao;
 
+	@Inject
+	private UsuarioDao usuarioDao;
+
+	private Usuario responsavel = new Usuario();
+
 	@Transacional
 	public void salva() {
 		this.bugDao.adiciona(bug);
@@ -33,6 +40,18 @@ public class BugBean {
 
 	public List<Status> getTodosOsStatus() {
 		return Arrays.asList(Status.values());
+	}
+
+	public List<Usuario> getUsuarios() {
+		return usuarioDao.lista();
+
+	}
+
+	public void adicionaResponsavel() {
+		this.responsavel = usuarioDao.busca(responsavel.getId());
+		this.bug.adiciona(responsavel);
+		this.responsavel = new Usuario();
+
 	}
 
 	public Bug getBug() {
@@ -49,6 +68,14 @@ public class BugBean {
 
 	public void setBugDao(BugDao bugDao) {
 		this.bugDao = bugDao;
+	}
+
+	public Usuario getResponsavel() {
+		return responsavel;
+	}
+
+	public void setResponsavel(Usuario responsavel) {
+		this.responsavel = responsavel;
 	}
 
 }
--- src/br/com/triadworks/bugtracker/modelo/Status.java
+++ src/br/com/triadworks/bugtracker/modelo/Status.java
@@ -2,16 +2,16 @@ package br.com.triadworks.bugtracker.modelo;
 
 public enum Status {
 
-	ABERTO("Aberto"),
-	FECHADO("Fechado");
-	
+	ABERTO("Aberto"), BLOQUEADO("Bloqueado"), FECHADO("Fechado");
+
 	private String descricao;
-	
+
 	private Status(String descricao) {
 		this.descricao = descricao;
 	}
-	
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 }
